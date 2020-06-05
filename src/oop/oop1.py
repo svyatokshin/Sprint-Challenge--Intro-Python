874b1d3f5c16076522e2c55e9fadba68a522231f
svyatoslav-okshin <svyatokshin@gmail.com>
2020-06-05 11:36:03 -0400

    finished oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,30 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():  # base class
+    pass
+
+
+class GroundVehicle(Vehicle):  # base class for Car and Motorcycle
+    pass
+
+
+class FlightVehicle(Vehicle):  # base class for Starship and Airplane
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
